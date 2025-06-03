f7789e8642d4b13e178d3b991c65879d30631cd2
pslow <mo.pr@outlook.com>
2025-06-03 14:09:41 +0200

    server GUI

--- desktop/server/include/window.h
+++ desktop/server/include/window.h
@@ -2,9 +2,50 @@
 #define WINDOW_H
 
 #include <QDialog>
+#include <QPushButton>
+#include <QSlider>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QCheckBox>
 
-class Window : public QDialog
+class window : public QDialog
 {
+    Q_OBJECT
+
+public:
+    window();
+    virtual ~window();
+
+private slots:
+    void onSpeedChanged(int val);
+    void onTemperatureChanged(int val);
+    void onBatteryChanged(int val);
+
+    void onLeftChecked(bool checked);
+    void onRightChecked(bool checked);
+    void onWarningChecked(bool checked);
+
+private:
+    QSlider speedSlider;
+    QSlider tempSlider;
+    QSlider batterySlider;
+
+    QLabel speedLabel{"Speed:"};
+    QLabel speedValueLabel{"0 Km/h"};
+
+    QLabel tempLabel{"Temperature:"};
+    QLabel tempValueLabel{"-60 °C"};
+
+    QLabel batteryLabel{"Battery:"};
+    QLabel batteryValueLabel{"0 %"};
+
+    QLabel lightSignalsLabel{"Light signals:"};
+    QCheckBox leftCheckBox{"Left"};
+    QCheckBox rightCheckBox{"Right"};
+    QCheckBox warningCheckBox{"Warning"};
+
+    QVBoxLayout layout;
 };
 
-#endif
+#endif // WINDOW_H
--- desktop/server/src/window.cpp
+++ desktop/server/src/window.cpp
@@ -1,2 +1,120 @@
 #include "window.h"
-#include "setting.h"
+//#include "setting.h"
+#include <QDebug>
+
+window::window()
+{
+    auto setupSlider = [](QSlider &slider, int min, int max)
+    {
+        slider.setOrientation(Qt::Horizontal);
+        slider.setRange(min, max);
+        slider.setValue(min);
+    };
+
+    setupSlider(speedSlider, 0, 240);
+    setupSlider(tempSlider, -60, 60);
+    setupSlider(batterySlider, 0, 100);
+
+    // Speed row
+    QHBoxLayout *speedLayout = new QHBoxLayout;
+    speedLayout->addWidget(&speedLabel);
+    speedLayout->addWidget(&speedSlider);
+    speedLayout->addWidget(&speedValueLabel);
+    layout.addLayout(speedLayout);
+
+    // Temperature row
+    QHBoxLayout *tempLayout = new QHBoxLayout;
+    tempLayout->addWidget(&tempLabel);
+    tempLayout->addWidget(&tempSlider);
+    tempLayout->addWidget(&tempValueLabel);
+    layout.addLayout(tempLayout);
+
+    // Battery row
+    QHBoxLayout *batteryLayout = new QHBoxLayout;
+    batteryLayout->addWidget(&batteryLabel);
+    batteryLayout->addWidget(&batterySlider);
+    batteryLayout->addWidget(&batteryValueLabel);
+    layout.addLayout(batteryLayout);
+
+    // Light signals row
+    QHBoxLayout *lightSignalsLayout = new QHBoxLayout;
+    lightSignalsLayout->addWidget(&lightSignalsLabel);
+    lightSignalsLayout->addWidget(&leftCheckBox);
+    lightSignalsLayout->addWidget(&rightCheckBox);
+    lightSignalsLayout->addWidget(&warningCheckBox);
+    layout.addLayout(lightSignalsLayout);
+
+    setLayout(&layout);
+    setWindowTitle("Server");
+    setFixedSize(1000, 200);
+
+    setWindowFlags(Qt::Window | Qt::WindowMinimizeButtonHint | Qt::WindowCloseButtonHint);
+
+    connect(&speedSlider, &QSlider::valueChanged, this, &window::onSpeedChanged);
+    connect(&tempSlider, &QSlider::valueChanged, this, &window::onTemperatureChanged);
+    connect(&batterySlider, &QSlider::valueChanged, this, &window::onBatteryChanged);
+
+    connect(&leftCheckBox, &QCheckBox::toggled, this, &window::onLeftChecked);
+    connect(&rightCheckBox, &QCheckBox::toggled, this, &window::onRightChecked);
+    connect(&warningCheckBox, &QCheckBox::toggled, this, &window::onWarningChecked);
+}
+
+window::~window() {}
+
+
+
+void window::onSpeedChanged(int val)
+{
+    speedValueLabel.setText(QString("%1 km/h").arg(val));
+    qDebug() << "Speed changed:" << val << "km/h";
+}
+
+void window::onTemperatureChanged(int val)
+{
+    tempValueLabel.setText(QString("%1 °C").arg(val));
+    qDebug() << "Temperature changed:" << val << "°C";
+}
+
+void window::onBatteryChanged(int val)
+{
+    batteryValueLabel.setText(QString("%1 %").arg(val));
+    qDebug() << "Battery changed:" << val << "%";
+}
+
+void window::onLeftChecked(bool checked)
+{
+    qDebug() << "Left signal" << (checked ? "checked" : "unchecked");
+    if (checked)
+    {
+        rightCheckBox.setChecked(false);
+        rightCheckBox.setEnabled(false);
+        warningCheckBox.setChecked(false);
+        warningCheckBox.setEnabled(true);
+    }
+    else
+    {
+        rightCheckBox.setEnabled(true);
+    }
+}
+
+void window::onRightChecked(bool checked)
+{
+    qDebug() << "Right signal" << (checked ? "checked" : "unchecked");
+    if (checked)
+    {
+        leftCheckBox.setChecked(false);
+        leftCheckBox.setEnabled(false);
+        warningCheckBox.setChecked(false);
+        warningCheckBox.setEnabled(true);
+    }
+    else
+    {
+        leftCheckBox.setEnabled(true);
+    }
+}
+
+void window::onWarningChecked(bool checked)
+{
+    qDebug() << "Warning signal" << (checked ? "checked" : "unchecked");
+    // No disabling of left or right checkboxes here
+}
