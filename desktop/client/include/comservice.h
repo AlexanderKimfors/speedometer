e9b6c546cce15fe419876561deeb3cf1add503ba
Alexander Kimfors <alexander.kimfors@hotmail.se>
2025-06-12 08:42:36 +0200

    added abstrakt functions for COMService.h

--- desktop/client/include/comservice.h
+++ desktop/client/include/comservice.h
@@ -3,6 +3,17 @@
 
 class COMService
 {
+public:
+    // Initiera kommunikation (öppna socket, port etc.)
+    virtual bool connect() = 0;
+
+    virtual bool sendData(const int &data) = 0;
+
+    virtual int receiveData() = 0;
+
+    virtual void disconnect() = 0;
+
+    virtual bool isConnected() const = 0;
 };
 
 #endif
