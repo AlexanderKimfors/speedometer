acc5dbb741598abb83b6cd6c5cc77146c31ad281
Alexander Kimfors <alexander.kimfors@hotmail.se>
2025-08-12 10:45:46 +0200

    removed TCP implementation

--- desktop/client/include/tcpservice.h
+++ desktop/client/include/tcpservice.h
@@ -3,27 +3,4 @@
 
 #include "comservice.h"
 
-constexpr int MESSAGE_LEN{8};
-
-class TCPService : public COMService
-{
-private:
-    int PORT;
-    int sockfd;
-    const char *SERVER_IP;
-
-public:
-    TCPService(int _PORT, char *_SERVER_IP) : PORT{_PORT}, SERVER_IP{_SERVER_IP} {}
-
-    bool connect() override;
-
-    bool sendData(const DataPacket &data) override;
-
-    bool receiveData(DataPacket &data) override;
-
-    void disconnect() override;
-
-    bool isConnected() const override;
-};
-
 #endif
