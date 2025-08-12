c77ec531f8ec400bd87efaaa197d470fb76af676
Alexander Kimfors <alexander.kimfors@hotmail.se>
2025-08-12 13:56:46 +0200

    updated main in both server and client

--- desktop/client/main.cpp
+++ desktop/client/main.cpp
@@ -1,8 +1,11 @@
 #include "window.h"
+#include "tcpservice.h"
 #include <QApplication>
 
 int main(int argc, char **argv)
 {
+    COMService *service{TCPService{}};
+
     QApplication app(argc, argv);
 
     Window window;
